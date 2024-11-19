1ff3864e29966dd3b8a108dbee74c6572220f47c
DevCAT <prototiphs0108@gmail.com>
2024-11-19 16:29:39 -0500

    Delete src directory

--- src/services/reportService.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:3000/api';
-
-export const generateReport = async (params) => {
-  const response = await axios.get(`${API_URL}/reports/generate`, { params });
-  return response.data;
-};
